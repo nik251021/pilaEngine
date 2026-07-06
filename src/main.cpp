df7db1e168daefc58b4d51b05db07a362b4ab85a
nik2510 <nikgvozdaryov@gmail.com>
2026-07-06 17:10:56 +0200

    Start

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (glfwInit() == GLFW_FALSE) {
+        glfwTerminate();
+        std::cout << "Error in initialising glfw";
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello pilaEngine", nullptr, nullptr);
+    if (window == NULL) {
+        glfwTerminate();
+        std::cout << "Error in creating window";
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GPU: " << glGetString(GL_RENDERER) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    return 0;
+}
