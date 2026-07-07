906a8e3a709dc474f28525a64b888082469dd3ea
nik2510 <nikgvozdaryov@gmail.com>
2026-07-07 14:32:51 +0200

    max fps added

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
 
 #include <iostream>
 #include <filesystem>
+#include <thread>
 
 #include "window.hpp"
 #include "other.hpp"
 
 namespace fs = std::filesystem;
+
+const double timePerFrame = 1.0/60.0;
 
 int main(int argc, char* argv[]) {
     fs::path pathToScene = argv[1];
@@ -27,6 +30,7 @@ int main(int argc, char* argv[]) {
     while (!window.shouldClose())
     {
         gameTime.update();
+        double start = glfwGetTime();
         std::cout << gameTime.deltaTime << std::endl;
         if (window.isKeyPressed(GLFW_KEY_ESCAPE)) {
             std::cout << "Escape, stopping" << std::endl;
@@ -34,6 +38,12 @@ int main(int argc, char* argv[]) {
         }
         std::cout << "Mouse X: [ " << window.getMousePos().x << " ] | Mouse Y: [ " << window.getMousePos().y << " ]" << std::endl;
         window.pollEvents();
+        double duration = glfwGetTime() - start;
+        if (duration < timePerFrame) {
+            double sleepDuration = timePerFrame - duration;
+            std::cout << "Sleep" << std::endl;
+            std::this_thread::sleep_for(std::chrono::duration<double>(sleepDuration));
+        }
     }
     glfwTerminate();
     std::cout << "Window was closed" << std::endl;
